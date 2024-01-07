bb6a50515fe35b31c9f009346ef37c78223b3a88
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-01-07 17:46:25 -0500

    Proyecto finalizado 071

--- first-Web-Springboot/src/main/java/com/adso/firstweb/FirstWebSpringbootApplication.java
+++ first-Web-Springboot/src/main/java/com/adso/firstweb/FirstWebSpringbootApplication.java
@@ -20,20 +20,4 @@ public class FirstWebSpringbootApplication{
         SpringApplication.run(FirstWebSpringbootApplication.class, args);
     }
 
-   /*
-    @Override
-    public void run(String... args) throws Exception {
-        //coment
-        personaService.crearPersona(new Persona(11L,"Ernio Hits",12));
-        personaService.crearPersona(new Persona(12L,"Camn loker",32));
-        personaService.crearPersona(new Persona(13L,"Urll Hangyek",111));
-        personaService.crearPersona(new Persona(14L,"Minda Kariak",33));
-
-        System.out.println("Personas Guardadas correctamente: " + personaService.contarPerson());
-
-        List<Persona> personas = personaService.obtenerToda();
-        personas.forEach(p -> System.out.println("Nombre Persona : "+ p.getNameUser()));
-    }
-    */
-
 }
--- first-Web-Springboot/src/main/java/com/adso/firstweb/controller/PersonaController.java
+++ first-Web-Springboot/src/main/java/com/adso/firstweb/controller/PersonaController.java
@@ -6,22 +6,10 @@ import com.adso.firstweb.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/*listarPersona: Maneja la solicitud GET en /personas para mostrar una lista de personas.
-Obtiene todas las personas del servicio PersonaService y las agrega al modelo con el nombre listpersonas, luego devuelve la vista listar.
-
-mostrarFormNewPerson: Maneja la solicitud GET en /personas/new para mostrar un formulario vacío para agregar una nueva persona.
-Crea una nueva instancia de Persona, la agrega al modelo como newPersona y establece el atributo accion en una cadena vacía. Devuelve la vista form.
-
-guardaNuevaPerson: Maneja la solicitud POST en /personas para guardar una nueva persona.
-Recibe los datos del formulario a través del objeto Persona,
-los pasa al servicio PersonaService para crear una nueva persona y luego redirige al usuario de vuelta a la lista de personas (redirect:/personas). */
 
 @RequestMapping("/personas")
 @Controller
@@ -30,7 +18,7 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
-    // tranferir del controller a la vista
+    //  el objeto Model se utiliza para pasar datos desde el controlador a la vista
     @GetMapping
     public String listarPersona (Model model){
         List<Persona> personas = personaService.obtenerToda();
@@ -45,10 +33,34 @@ public class PersonaController {
         return "form";
     }
 
+    /*@ModelAttribute se usa para enlazar datos de la solicitud a un objeto Java
+    o para devolver atributos del modelo en métodos controladores,
+    facilitando la transferencia de datos entre el controlador y la vista en Spring MVC.*/
+
     @PostMapping("/new")
     public String guardaNuevaPerson(@ModelAttribute Persona persona){
         personaService.crearPersona(persona);
         return "redirect:/personas";
     }
+
+    @GetMapping("/editar/{idUser}")
+    public String mostrarFormEditPerson(@PathVariable Long idUser,@ModelAttribute Persona persona, Model model){
+        model.addAttribute("newPersona",persona);
+        model.addAttribute("accion","/personas/editar/"+idUser);
+        return "form";
+    }
+
+    @PostMapping("/editar/{idUser}")
+    public String actualizarPerson(@PathVariable Long idUser, @ModelAttribute Persona persona){
+        personaService.actualizarPerson(idUser,persona);
+        return "redirect:/personas";
+    }
+
+    @GetMapping("/eliminar/{idUser}")
+    public String eliminarPerson(@PathVariable Long idUser){
+        personaService.eliminarPerson(idUser);
+        return "redirect:/personas";
+    }
+
 
 }
